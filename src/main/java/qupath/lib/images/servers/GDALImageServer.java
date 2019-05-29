b4a373f22cb2b778e55033c0d6574151c6a78dee
Fred F <f--f@users.noreply.github.com>
2019-05-29 12:47:40 -0400

    Insert GPL3 copyright notice into source code

--- src/main/java/qupath/lib/images/servers/GDALImageServer.java
+++ src/main/java/qupath/lib/images/servers/GDALImageServer.java
@@ -1,3 +1,25 @@
+/*
+Copyright (C) 2019 STTARR, University Health Network
+Contact: STTARR (STTARR.Image.Analysis@uhn.ca)
+
+This project is derived from and extends QuPath <http://qupath.github.io>.
+Copyright (C) 2014 - 2016 The Queen's University of Belfast, Northern Ireland
+Contact: IP Management (ipmanagement@qub.ac.uk)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package qupath.lib.images.servers;
 
 import java.awt.Graphics2D;
